5d30e5ad742a888b56a46df999d2d0a3c78ecefa
DylanRR <dragaishis@gmail.com>
2025-01-02 14:20:57 -0600

    Refactor MAX30102 sensor integration and enhance communication handling

--- include/static.h
+++ include/static.h
@@ -8,12 +8,25 @@
 #define OLED_DC    49  // D/C
 #define OLED_RESET 48  // /RES
 
-// Pin definitions for the MAX30102 sensor
-#define MAX30102_INT 3
 
-// Global Timer variable
-unsigned long timer = 0;
 
 // Global variables for the MAX30102 sensor
-#define FINGER_THRESHOLD 60000
-bool transmitting = false;
+#define FINGER_THRESHOLD 60000    // Threshold value for detecting a finger
+#define MAX30102_INT 3            // Interrupt pin for the MAX30102 sensor 
+
+// Global variables for serial communication
+bool transmitting = false;        // Flag to indicate if the device is currently transmitting data over serial
+unsigned long timer = 0;          // Timer used for sending SYNC bytes
+#define SYNC_TIME 500             // Time to wait between sending SYNC bytes
+#define SYN 0x16                  // Hexadecimal code for ASCII Synchronous Idle
+#define STX 0x02                  // Hexadecimal code for ASCII Start of Text
+#define ETX 0x03                  // Hexadecimal code for ASCII End of Text
+#define RS 0x1E                   // Hexadecimal code for ASCII Record Separator
+
+// Global Pin definitions
+#define DISPLAY_COMMUNICATION_PIN 6
+#define SERIAL_COMMUNICATION_PIN 7
+#define FINGER_DETECTED_LED 22
+#define HEART_RATE_DETECTED_LED 24
+#define SERIAL_OUTPUT_LED 26
+#define DISPLAY_OUTPUT_LED 28
--- src/main.cpp
+++ src/main.cpp
@@ -11,31 +11,93 @@ Sensor_Manager sensorManager(MAX30102_INT, FINGER_THRESHOLD);
 void RUN_STATE();
 void IDLE_STATE();
 void debugPrint(String location, String message = "");
+bool setup_determineCommunicationType();
+void DC_MAIN_LOOP();
+void DC_RUN_STATE();
+void DC_IDLE_STATE();
+void SC_MAIN_LOOP();
+byte int32ToByte(int32_t value);
+
+bool communicationType;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting setup...");
+  pinMode(SERIAL_OUTPUT_LED, OUTPUT);
+  pinMode(DISPLAY_OUTPUT_LED, OUTPUT);
+  communicationType = setup_determineCommunicationType();
+  pinMode(FINGER_DETECTED_LED, OUTPUT);
+  pinMode(HEART_RATE_DETECTED_LED, OUTPUT);
+  
   sensorManager.init();
-  sensorManager.setDataRamping(true);
-  Serial.println("MAX30102 sensor initialized...");
-  screenManager.init();
-  Serial.println("Display initialized...");
-  screenManager.init();
-  screenManager.setState(0);
-  Serial.println("Screen manager initialized and state set...");
-  Serial.println("Setup complete...");
+
+  if (communicationType) {
+    sensorManager.setDataRamping(true);
+    screenManager.init();
+    screenManager.setState(0);
+  }
+  else {
+    sensorManager.setDataRamping(false);
+  }
+}
+
+bool setup_determineCommunicationType() {
+  pinMode(DISPLAY_COMMUNICATION_PIN, INPUT);
+  pinMode(SERIAL_COMMUNICATION_PIN, INPUT);
+  bool DCP = digitalRead(DISPLAY_COMMUNICATION_PIN);
+  bool SCP = digitalRead(SERIAL_COMMUNICATION_PIN);
+  if (DCP && SCP) {
+    while (true) {
+      digitalWrite(DISPLAY_OUTPUT_LED, HIGH);
+      digitalWrite(SERIAL_OUTPUT_LED, HIGH);
+      delay(500);
+      digitalWrite(DISPLAY_OUTPUT_LED, LOW);
+      digitalWrite(SERIAL_OUTPUT_LED, LOW);
+      delay(500);
+    }
+  }
+  else if (!DCP && !SCP) {
+    while (true) {
+      digitalWrite(DISPLAY_OUTPUT_LED, HIGH);
+      delay(250);
+      digitalWrite(DISPLAY_OUTPUT_LED, LOW);
+      delay(250);
+      digitalWrite(SERIAL_OUTPUT_LED, HIGH);
+      delay(250);
+      digitalWrite(SERIAL_OUTPUT_LED, LOW);
+      delay(250);
+    }
+  }
+  else if (DCP) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 void loop() {
-  if (sensorManager.isFingerDetected()) {
-    RUN_STATE();
+  if (communicationType) {
+    while (true) {
+      DC_MAIN_LOOP();
+    }
   }
   else {
-    IDLE_STATE();
+    while (true) {
+      SC_MAIN_LOOP();
+    }
   }
 }
 
-void RUN_STATE(){
+void DC_MAIN_LOOP() {
+  if (sensorManager.isFingerDetected()) {
+    DC_RUN_STATE();
+  }
+  else {
+    DC_IDLE_STATE();
+  }
+}
+
+void DC_RUN_STATE(){
   if (screenManager.getActiveState() == 0) {
       screenManager.setState(1);
   }
@@ -53,13 +115,51 @@ void RUN_STATE(){
   }
 }
 
-void IDLE_STATE(){
+void DC_IDLE_STATE(){
   if (screenManager.getActiveState() != 0) {
     screenManager.setState(0);
     sensorManager.reset();
   }
 }
 
+bool SC_transmitting = false;
+void SC_MAIN_LOOP() {
+  if (sensorManager.isFingerDetected()) {
+    if (!SC_transmitting) {
+      Serial.write(STX);
+      SC_transmitting = true;
+    }
+    int32_t temp_HR;
+    temp_HR = sensorManager.getHeartRate();
+    sensorManager.update();
+    int32_t new_HR;
+    new_HR = sensorManager.getHeartRate();
+    if (temp_HR != new_HR) {
+      Serial.write(int32ToByte(new_HR));
+      Serial.write(RS);
+    }
+  }
+  else {
+    if (SC_transmitting) {
+      Serial.write(ETX);
+      SC_transmitting = false;
+    }
+    if (!SC_transmitting && millis() - timer > SYNC_TIME) {
+      Serial.write(SYN);
+      timer = millis();
+    }
+  }
+}
+
+byte int32ToByte(int32_t value) {
+  if (value < 0) {
+    return 0; // Round to minimum value of a byte
+  } else if (value > 255) {
+    return 255; // Round to maximum value of a byte
+  } else {
+    return (byte)value; // Cast to byte
+  }
+}
 
 
 void debugPrint(String message, String location = "") {
